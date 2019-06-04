1059e9fc3a35fec30c664381536b3a761b9fcc6d
ZackDinerstein <zackdhome@gmail.com>
2019-06-04 11:50:45 +0300

    updated eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,28 @@
 module.exports = {
+    "parser": "babel-eslint",
     "env": {
         "browser": true,
         "es6": true
     },
-    "extends": "airbnb",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    "settings": {
+          "ecmascript": 6,
+          "jsx": true
     },
     "parserOptions": {
+        "ecmaVersion": 2017,
         "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "experimentalDecorators": true,
             "jsx": true
         },
-        "ecmaVersion": 2018
+        "sourceType": "module"
     },
     "plugins": [
-        "react"
+        "react",
     ],
+    "extends": "airbnb",
     "rules": {
+      "react/jsx-filename-extension": 0,
+      "function-paren-newline": 0
     }
-};
+  };
